e24d931770885e65d0923f81549f1560bfea24a7
Matt Bessey <bessey@gmail.com>
2012-04-01 21:37:17 +0100

    Precompiled Assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,32 +1,33 @@
-require 'bundler/capistrano'
-set :user, 'ec2-user'
-set :domain, 'sngtrkr.com'
-set :applicationdir, "/home/ec2-user/sngtrkr/"
- 
-set :scm, 'git'
-set :repository,  "ssh://ec2-user@sngtrkr.com/~/sngtrkr.git"
-#set :git_enable_submodules, 0 # if you have vendored rails
-set :branch, 'master'
-set :git_shallow_clone, 1
-set :scm_verbose, true
- 
-# roles (servers)
-role :web, domain
-role :app, domain
-role :db,  domain, :primary => true
- 
-# deploy config
-set :deploy_to, applicationdir
-set :deploy_via, :export
+# The name of your app
+set :application, "sngtrkr"
+# The directory on the EC2 node that will be deployed to
+set :deploy_to, "/var/www/apps/#{application}"
+# The type of Source Code Management system you are using
+set :scm, :git
+# The location of the LOCAL repository relative to the current app
+set :repository,  "."
+# The way in which files will be transferred from repository to remote host
+# If you were using a hosted github repository this would be slightly different
+set :deploy_via, :copy
 
-# additional settings
-default_run_options[:pty] = true  # Forgo errors when deploying from windows
-ssh_options[:keys] = %w(~/Dropbox/SNGTRKR/mattbillyhosts.pem)            
-# If you are using 
-#ssh_keyset :chmod755, "app config db lib public vendor script script/* public/disp*"
-set :use_sudo, false
+# The address of the remote host on EC2 (the Public DNS address)
+set :location, "sngtrkr.com"
+# setup some Capistrano roles
+role :app, location
+role :web, location
+role :db,  location, :primary => true
 
-# Passenger
+# Set up SSH so it can connect to the EC2 node - assumes your SSH key is in ~/.ssh/id_rsa
+set :user, "ec2-user"
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+set :synchronous_connect, true
+
+before "deploy:assets:precompile", "bundle:install"
+
+# If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
