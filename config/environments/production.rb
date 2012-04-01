ee09211e876114627732259b90e9f6443331a5c2
Matt Bessey <bessey@gmail.com>
2012-04-01 22:03:56 +0100

    Adding debuggin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 SNGTRKRR::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+# Settings specified here will take precedence over those in config/application.rb
 
-  # Code is not reloaded between requests
+# Code is not reloaded between requests
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
@@ -16,6 +16,9 @@ SNGTRKRR::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+
+  # Debugging on production
+  config.assets.debug = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -64,7 +67,7 @@ SNGTRKRR::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  
+
   config.assets.precompile += %w( *.css *.js )
 
 end
