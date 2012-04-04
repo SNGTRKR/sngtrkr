211a81dd6f6fe1a3e700a887750b4eb1dc543945
Matt Bessey <bessey@gmail.com>
2012-04-04 02:31:16 +0100

    Basic 7digital search implemented. Need to work further to get to point of adding all releases.

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -13,7 +13,9 @@ class Scraper
   end
 
   def self.sevenDigitalSearch search
-
+    sevendigital_apikey = "7dufgm34849u";
+    xml =  Hash.from_xml Net::HTTP.get( URI.parse("http://api.7digital.com/1.2/artist/search?q=#{search}&sort=score%20desc&oauth_consumer_key=#{sevendigital_apikey}"))
+    return xml["response"]["searchResults"]["searchResult"][0]
   end
 
   def self.lastFmSearch search
