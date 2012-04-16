d485fc0c34f169154411cc18b353b3ec9ae9bb41
Matt Bessey <bessey@gmail.com>
2012-04-16 02:51:15 +0100

    Deploy changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ SNGTRKRR::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Debugging on production
   config.assets.debug = true
