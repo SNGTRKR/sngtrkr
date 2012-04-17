df7c6b6e0ca2924eb1b27b005605537bcbb09cbd
Matt Bessey <bessey@gmail.com>
2012-04-17 23:55:46 +0100

    D

--- config/deploy.rb
+++ config/deploy.rb
@@ -72,15 +72,24 @@ namespace :deploy do
   #run "cd #{latest_release} && #{rake} queue:restart_workers RAILS_ENV=production"
 end
 
-namespace :deploy do
-  desc "Hot-reload God configuration for the Resque worker"
-  task :reload_god_config do
-    run " ~/.rvm/gems/ruby-1.9.3-p125/bin/god stop resque"
-    run " ~/.rvm/gems/ruby-1.9.3-p125/bin/god load #{File.join(deploy_to, 'current', 'config', 'god','resque-' + rails_env + '.god')}"
-    run " ~/.rvm/gems/ruby-1.9.3-p125/bin/god start resque"
-  end
+after "deploy:symlink", "deploy:restart_workers"
+
+##
+# Rake helper task.
+# http://pastie.org/255489
+# http://geminstallthat.wordpress.com/2008/01/27/rake-tasks-through-capistrano/
+# http://ananelson.com/said/on/2007/12/30/remote-rake-tasks-with-capistrano/
+def run_remote_rake(rake_cmd)
+  rake_args = ENV['RAKE_ARGS'].to_s.split(',')
+  cmd = "cd #{fetch(:latest_release)} && #{fetch(:rake, "rake")} RAILS_ENV=#{fetch(:rails_env, "production")} #{rake_cmd}"
+  cmd += "['#{rake_args.join("','")}']" unless rake_args.empty?
+  run cmd
+  set :rakefile, nil if exists?(:rakefile)
 end
 
-# append to the bottom:
-
-after :deploy, "deploy:reload_god_config"
+namespace :deploy do
+  desc "Restart Resque Workers"
+  task :restart_workers, :roles => :db do
+    run_remote_rake "resque:restart_workers"
+  end
+end
