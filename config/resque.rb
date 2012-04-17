b573aa0dbfc6fe03ec5138daffa1be90235480d4
Matt Bessey <bessey@gmail.com>
2012-04-18 00:45:34 +0100

    D

--- config/resque.rb
+++ config/resque.rb
@@ -1,18 +1,6 @@
-# Start a worker with proper env vars and output redirection
-def run_worker(queue, count = 1)
-  puts "Starting #{count} worker(s) with QUEUE: #{queue}"
-  ops = {:pgroup => true, :err => [(Rails.root + "log/resque_err").to_s, "a"], 
-                          :out => [(Rails.root + "log/resque_stdout").to_s, "a"]}
-  env_vars = {"QUEUE" => queue.to_s}
-  count.times {
-    ## Using Kernel.spawn and Process.detach because regular system() call would
-    ## cause the processes to quit when capistrano finishes
-    pid = spawn(env_vars, "rake resque:work", ops)
-    Process.detach(pid)
-  }
-end
+require 'resque/tasks'
 
-namespace :resque do
+namespace :resque do  
   task :setup => :environment
 
   desc "Restart running workers"
@@ -23,22 +11,66 @@ namespace :resque do
   
   desc "Quit running workers"
   task :stop_workers => :environment do
-    pids = Array.new
-    Resque.workers.each do |worker|
-      pids.concat(worker.worker_pids)
+    stop_workers
+  end
+  
+  desc "Start workers"
+  task :start_workers => :environment do    
+    run_worker("*", 1)
+  end
+  
+  
+  
+  def store_pids(pids, mode)
+    pids_to_store = pids
+    pids_to_store += read_pids if mode == :append
+
+    # Make sure the pid file is writable.    
+    File.open(File.expand_path('tmp/pids/resque.pid', Rails.root), 'w') do |f|
+      f <<  pids_to_store.join(',')
     end
+  end
+
+  def read_pids
+    pid_file_path = File.expand_path('tmp/pids/resque.pid', Rails.root)
+    return []  if ! File.exists?(pid_file_path)
+    
+    File.open(pid_file_path, 'r') do |f| 
+      f.read 
+    end.split(',').collect {|p| p.to_i }
+  end
+
+  def stop_workers
+    pids = read_pids
+    
     if pids.empty?
       puts "No workers to kill"
     else
       syscmd = "kill -s QUIT #{pids.join(' ')}"
-      puts "Running syscmd: #{syscmd}"
-      system(syscmd)
+      puts "$ #{syscmd}"
+      `#{syscmd}`
+      store_pids([], :write)
     end
   end
   
-  desc "Start workers"
-  task :start_workers => :environment do
-    run_worker("*", 2)
-    #run_worker("high", 1)
+  # Start a worker with proper env vars and output redirection
+  def run_worker(queue, count = 1)
+    puts "Starting #{count} worker(s) with QUEUE: #{queue}"
+
+    ##  make sure log/resque_err, log/resque_stdout are writable.
+    ops = {:pgroup => true, :err => [(Rails.root + "log/resque_err").to_s, "a"], 
+                            :out => [(Rails.root + "log/resque_stdout").to_s, "a"]}
+    env_vars = {"QUEUE" => queue.to_s, 'RAILS_ENV' => Rails.env.to_s}
+
+    pids = []
+    count.times do
+      ## Using Kernel.spawn and Process.detach because regular system() call would
+      ## cause the processes to quit when capistrano finishes
+      pid = spawn(env_vars, "rake resque:work", ops)
+      Process.detach(pid)
+      pids << pid
+    end
+    
+    store_pids(pids, :append)
   end
 end
