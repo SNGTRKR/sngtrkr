3b11d1d9aa2498fcbfa70b18af0edc320271a793
Matt Bessey <bessey@gmail.com>
2012-04-20 19:51:28 +0100

    Fix facebook auth

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,6 @@ if defined?(Bundler)
 # Bundler.require(:default, :assets, Rails.env)
 end
 
-ActionDispatch::Callbacks.after do
-  # Reload the factories
-  return unless (Rails.env.development? || Rails.env.test?)
-
-  unless FactoryGirl.factories.blank? # first init will load factories, this should only run on subsequent reloads
-    FactoryGirl.factories.clear
-    FactoryGirl.find_definitions
-  end
-end
 
 module SNGTRKRR
   class Application < Rails::Application
