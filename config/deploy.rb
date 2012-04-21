06e3788a3d7aa233edc9625ba84a38cd001cad51
ec2-user <bessey@gmail.com>
2012-04-21 19:36:53 +0100

    Fuck it, back to the usual.

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,6 +52,22 @@ namespace :deploy do
   end
 end
 
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+    task :precompilef do
+      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+    end
+  end
+end
+
 after "deploy:restart", "delayed_job:restart"
 
 namespace :delayed_job do 
