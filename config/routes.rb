d5d1c205336e8a9e0f37612da1217fc72a8f77bb
ec2-user <bessey@gmail.com>
2012-04-21 23:10:49 +0100

    Fixed omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,12 @@ SNGTRKRR::Application.routes.draw do
   match '/recommended' => "Pages#recommended"
   match '/home' => "Pages#home"
   match '/splash' => "Pages#splash"
-#  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin' # Feel free to change '/admin' to any namespace you need.
+  #  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin' # Feel free to change '/admin' to any namespace you need.
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users_controller/omniauth_callbacks" }
-
+  devise_scope :user do
+    get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
+  end
   match 'users/me' => 'users#self'
   # Use this address through AJAX to import all a user's facebook artists.
   resources :users
