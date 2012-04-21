14cd3b836dfcadb5ad45a9e2d2fd82df7ad01741
ec2-user <bessey@gmail.com>
2012-04-21 23:11:37 +0100

    Fixed login through facebook

--- app/controllers/users_controller/omniauth_callbacks_controller.rb
+++ app/controllers/users_controller/omniauth_callbacks_controller.rb
@@ -6,8 +6,7 @@ class UsersController::OmniauthCallbacksController < Devise::OmniauthCallbacksCo
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
       #test
-      sign_in @user, :event => :authentication
-      redirect_to "/"
+      sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
