841ba04e1b73cf39d035b46895344e143af93766
ec2-user <bessey@gmail.com>
2012-04-28 20:49:39 +0100

    Fixed artist search

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -22,7 +22,7 @@ class Artist < ActiveRecord::Base
 
   def self.search(search)
     if search
-      #find(:all, :conditions => ["name LIKE '%%#{search}%%'"])
+      find(:all, :conditions => ["name LIKE '%%#{search}%%'"])
     else
       self.all
     end
