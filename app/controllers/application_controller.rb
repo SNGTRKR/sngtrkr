9f5273c35a97c2ea49775838f8f20026d5d9a9ab
Tom Dallimore <tomdallimore@Tom-Dallimores-MacBook-Pro.local>
2012-05-06 19:55:00 +0100

    Updated 404 and 500 routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,27 @@
-class Error404 < StandardError; end
-
 class ApplicationController < ActionController::Base
-  protect_from_forgery
 
-  rescue_from Error404, :with => :render_404
-  def render_404
-    respond_to do |type|
-      type.html { render :template => "errors/error_404", :status => 404, :layout => 'error' }
-      type.all  { render :nothing => true, :status => 404 }
+unless Rails.application.config.consider_all_requests_local
+    rescue_from Exception, with: :render_500
+    rescue_from ActionController::RoutingError, with: :render_404
+    rescue_from ActionController::UnknownController, with: :render_404
+    rescue_from ActionController::UnknownAction, with: :render_404
+    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  end
+
+  private
+  def render_404(exception)
+    @not_found_path = exception.message
+    respond_to do |format|
+      format.html { render template: 'errors/error_404', layout: 'layouts/application', status: 404 }
+      format.all { render nothing: true, status: 404 }
     end
-    true
+  end
+
+  def render_500(exception)
+    @error = exception
+    respond_to do |format|
+      format.html { render template: 'errors/error_500', layout: 'layouts/application', status: 500 }
+      format.all { render nothing: true, status: 500}
+    end
   end
 end
