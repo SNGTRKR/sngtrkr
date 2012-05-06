d4c789d8e91d1dcf0167d9d1578f65dbd24c25ed
ec2-user <bessey@gmail.com>
2012-05-06 03:50:15 +0100

    404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from Error404, :with => :render_404
+  def render_404
+    respond_to do |type|
+      type.html { render :template => "errors/error_404", :status => 404, :layout => 'error' }
+      type.all  { render :nothing => true, :status => 404 }
+    end
+    true
+  end
 end
