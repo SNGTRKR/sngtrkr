8a626f63e4991260f2de97396048f37a8647250c
ec2-user <bessey@gmail.com>
2012-05-11 00:07:47 +0100

    Db migration changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120505004132) do
+ActiveRecord::Schema.define(:version => 20120510230241) do
 
   create_table "artists", :force => true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20120505004132) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "itunes"
   end
 
   create_table "delayed_jobs", :force => true do |t|
