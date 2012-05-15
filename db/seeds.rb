07be756f12e92b040a7bf5997de7591b17ae5f82
Tom Dallimore <tomdallimore@unknown-e0-f8-47-1a-3b-f8.home>
2012-05-15 17:58:06 +0100

    Added individual friend page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
-User.create!(:first_name => 'Billy', :last_name => 'Dallimore', :fbid => "660815460", :email => "test@tom.com",:password => 'test42343egysfdf')
+User.create!(:first_name => 'Billy', :last_name => 'Dallimore', :fbid => "660815460", :email => "tom.alan.dallimore@googlemail.com",:password => 'test42343egysfdf')
 User.create!(:first_name => 'Matt', :last_name => 'Bessey', :fbid => "757635703", :email => "bessey@gmail.com",:password => 'test42343egy20df')
 User.create!(:first_name => 'Barry', :last_name => 'Smith', :fbid => "123456789", :email => "test@private.com",:password => 'test42343egy76df')
