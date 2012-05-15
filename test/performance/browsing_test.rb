3d5865103451af9ffc5e8cec2ad691d7d2a1774a
ec2-user <bessey@gmail.com>
2012-05-15 20:19:30 +0100

    List of friends in JS api implemented

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -5,11 +5,16 @@ class BrowsingTest < ActionDispatch::PerformanceTest
   # Refer to the documentation for all available options
   # self.profile_options = { :runs => 5, :metrics => [:wall_time, :memory]
   #                          :output => 'tmp/performance', :formats => [:flat] }
-
   def test_homepage
     get '/'
   end
+
   def test_artist
     get '/artists/1'
+  end
+
+  def test_logged_in
+    #sign_in @user
+    get '/users/self'
   end
 end
