8e9ac1a383e9da8a527300ac7e722f60472c66fa
ec2-user <bessey@gmail.com>
2012-05-17 02:07:43 +0100

    Foundations for tracks added

--- db/migrate/20120517010154_create_tracks.rb
+++ db/migrate/20120517010154_create_tracks.rb
@@ -1,9 +1,8 @@
 class CreateTracks < ActiveRecord::Migration
   def change
     create_table :tracks do |t|
-      t.string :name
-      t.integer :number
-      t.integer :release_id
+      t.text :name
+      t.text :sd_preview
 
       t.timestamps
     end
