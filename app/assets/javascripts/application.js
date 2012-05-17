5f0c7743a32c785fdce179ab4e6883397358d777
ec2-user <bessey@gmail.com>
2012-05-17 03:11:26 +0100

    javascript order swap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require jquery.min
 //= require jquery_ujs
-//= require jquery
 $(document).ready(function() {
 	$("#delete-account a").fancybox({
 		width : 431,
