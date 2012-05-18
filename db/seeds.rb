cfa943e735b58ba434903a637c9067ebb719374e
Tom Dallimore <tomdallimore@unknown-e0-f8-47-1a-3b-f8.home>
2012-05-18 02:48:03 +0100

    Formatted manage artist page

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 User.create!(:first_name => 'Billy', :last_name => 'Dallimore', :fbid => "660815460", :email => "tom.alan.dallimore@googlemail.com",:password => 'test42343egysfdf')
 User.create!(:first_name => 'Matt', :last_name => 'Bessey', :fbid => "757635703", :email => "bessey@gmail.com",:password => 'test42343egy20df')
 User.create!(:first_name => 'Barry', :last_name => 'Smith', :fbid => "123456789", :email => "test@private.com",:password => 'test42343egy76df')
+Artist.create!( :id => '25', :name =>"JellyFishBoy", :fbid => "204842319549165", :genre => "Dubstep", :hometown => "Bristol", :booking_email => "billy@jellyfishboy.co.uk")
