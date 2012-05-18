a5544ea3a9973e199100dffffcacad81a70f5855
ec2-user <bessey@gmail.com>
2012-05-18 04:59:26 +0100

    Page rendering times and team

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,12 @@ class ApplicationController < ActionController::Base
   #  rescue_from ActiveRecord::RecordNotFound, with: :render_404
   #end
   before_filter :authenticate_user!, :except => [:splash]
+  before_filter :timer_start
+  
+  def timer_start
+    @start_time = Time.now
+  end
+  
   def default_url_options
     if Rails.env.production?
       {:host => "sngtrkr.com"}
