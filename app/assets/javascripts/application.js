a376497ba3479b6196a024d9788f8401c30cd813
Tom Dallimore <tomdallimore@unknown-e0-f8-47-1a-3b-f8.home>
2012-05-19 18:32:57 +0100

    Manage form edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,46 +19,6 @@ $(document).ready(function() {
 		autoSize	: false,
 		closeClick	: false
 	});
-    		$(".manage-artist").click(function(){
-  			 $(".step-back").animate({height:134},"slow");
-  			 $(".step").animate({height:222},"slow");	
-  			 $("#progress").animate({width:65},"slow");
-  			 $('.percent').html('25%');	 
-  			 $('.white-tint').hide();
-  			 $('.white-tint-2').show();
-  			 $('.white-tint-3').show();
-  			 $('.white-tint-4').show();
-});
-$(".manage-details").click(function(){
-  			 $(".step-back").animate({height:421},"slow");
-  			 $(".step").animate({height:505},"slow");
-  			 $("#progress").animate({width:130},"slow");
-  			 $('.percent').html('50%');
-  			 $('.white-tint-2').hide();
-  			 $('.white-tint').show();
-  			 $('.white-tint-3').show();
-  			 $('.white-tint-4').show();
-});
-$(".manage-picture").click(function(){
-  			 $(".step-back").animate({height:187},"slow");
-  			 $(".step").animate({height:275},"slow");
-  			 $("#progress").animate({width:195},"slow");
-  			 $('.percent').html('75%');
-  			 $('.white-tint-3').hide();
-  			 $('.white-tint-2').show();
-  			 $('.white-tint').show();
-  			 $('.white-tint-4').show();
-});
-$(".manage-summary").click(function(){
-  			 $(".step-back").animate({height:283},"slow");
-  			 $(".step").animate({height:371},"slow");
-  			 $("#progress").animate({width:265},"slow");
-  			 $('.percent').html('100%');
-  			 $('.white-tint-4').hide();
-  			 $('.white-tint-2').show();
-  			 $('.white-tint-3').show();
-  			 $('.white-tint').show();
-});
     	});
     	
 
