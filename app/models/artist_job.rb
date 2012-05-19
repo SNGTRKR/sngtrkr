d202cc5fa8dc847cb06b47d9383c3cbed8425b96
ec2-user <bessey@gmail.com>
2012-05-19 23:20:00 +0100

    Job fixing in progress

--- app/models/artist_job.rb
+++ app/models/artist_job.rb
@@ -5,19 +5,17 @@ class ArtistJob
     require 'open-uri'
     graph = Koala::Facebook::API.new(access_token)
     music = graph.get_connections("me", "music?fields=name")
-    i = 0
     artist_ids = music.each do |artist|
       artist["id"]
     end
     Koala::Facebook::BatchOperation.instance_variable_set(:@identifier, 0)
     results = []
     artist_ids.in_groups_of(50) do |artists|
-      i=0
-      results = graph.batch do |batch_api|
+      tmp_results = graph.batch do |batch_api|
         for artist in artists do
           if(artist.nil?)
           Rails.logger.error "J001: Failed, batch api request returned nil."
-          break
+          next
           end
           # TODO: DISABLE FOR PRODUCTION
           tmp = Artist.where("fbid = ?",artist["id"]).first
@@ -27,9 +25,9 @@ class ArtistJob
           next
           end
           batch_api.get_object(artist["id"]+'?fields=name,general_manager,booking_agent,record_label,genre,hometown,website,bio')
-          i=i+1
         end
       end
+      results = results | tmp_results
       if Rails.env.development?
       # Limits the intake of artists to 50 when developing.
       break
@@ -41,7 +39,7 @@ class ArtistJob
     end
     end_time = Time.now
     elapsed_time = end_time - start_time
-    Rails.logger.info "J001: Artist initial import delayed job after #{elapsed_time}"
+    Rails.logger.info "J001: Artist initial import delayed job after #{elapsed_time}, #{results.count} artists imported"
     return true
   end
 end
