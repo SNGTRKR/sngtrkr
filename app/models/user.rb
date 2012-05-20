a1f36448e5f54f2a9770a81d8b01885b74e08c25
ec2-user <bessey@gmail.com>
2012-05-20 14:39:19 +0100

    Started work on admin accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :fbid, :first_name, :last_name, :last_sign_in_at
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :fbid, :first_name, :last_name, :last_sign_in_at, :admin
 
   has_many :follow
   has_many :suggest
