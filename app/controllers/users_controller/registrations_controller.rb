30415aa3fb3fa0f891c2e985e56cff8e28846208
ec2-user <bessey@gmail.com>
2012-05-21 14:23:14 +0100

    Adjusted registrations controller

--- app/controllers/users_controller/registrations_controller.rb
+++ app/controllers/users_controller/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Users::RegistrationsController < Devise::RegistrationsController
+class UsersController::RegistrationsController < Devise::RegistrationsController
   before_filter :check_permissions, :only => [:new, :create, :cancel, :timeline]
   skip_before_filter :require_no_authentication
  
