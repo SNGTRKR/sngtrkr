e9fb5e35ac9cb1d614ac6f63c8a80ecf361511c7
ec2-user <bessey@gmail.com>
2012-05-22 00:24:40 +0100

    Adjusted welcome mailer to not crash when email not verified

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   default from: "noreply@sngtrkr.com"
+  ses = AWS::SES::Base.new()
   def welcome_email(user)
-    @user = user
-    mail(:to => user.email, :subject => "Welcome to SNGTRKR!")
+    mail(:to => user.email, :subject => "Welcome to SNGTRKR!") rescue Rails.logger.error("Welcome Email Failed to Send")
   end
 end
