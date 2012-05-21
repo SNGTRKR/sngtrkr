9a5bd9c1b86c439fc8594334f1c910f6adbff80b
ec2-user <bessey@gmail.com>
2012-05-22 00:34:42 +0100

    Verify user before emailing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,11 @@
 class UserMailer < ActionMailer::Base
   default from: "noreply@sngtrkr.com"
-  #ses = AWS::SES::Base.new(
-  #:access_key_id     => ENV['AMAZON_ACCESS_KEY'],
-  #:secret_access_key => ENV['AMAZON_SECRET_KEY']
-  #)
+  ses = AWS::SES::Base.new(
+  :access_key_id     => ENV['AMAZON_ACCESS_KEY'],
+  :secret_access_key => ENV['AMAZON_SECRET_KEY']
+  )
   def welcome_email(user)
-    mail(:to => user.email, :subject => "Welcome to SNGTRKR!") rescue Rails.logger.error("Welcome Email Failed to Send")
+    ses.addresses.verify(user.email)
+    mail(:to => user.email, :subject => "Welcome to SNGTRKR!")
   end
 end
