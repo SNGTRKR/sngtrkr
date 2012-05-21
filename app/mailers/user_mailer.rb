f8d53812cb554c53b7c7d4c071f97bb9f6d65406
ec2-user <bessey@gmail.com>
2012-05-22 00:30:00 +0100

    Removing unneeded code from mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,9 @@
 class UserMailer < ActionMailer::Base
   default from: "noreply@sngtrkr.com"
-  ses = AWS::SES::Base.new()
+  #ses = AWS::SES::Base.new(
+  #:access_key_id     => ENV['AMAZON_ACCESS_KEY'],
+  #:secret_access_key => ENV['AMAZON_SECRET_KEY']
+  #)
   def welcome_email(user)
     mail(:to => user.email, :subject => "Welcome to SNGTRKR!") rescue Rails.logger.error("Welcome Email Failed to Send")
   end
