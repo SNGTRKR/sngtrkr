c3e4523cb8e52af2b52a4e9c4e97f669dc81069a
ec2-user <bessey@gmail.com>
2012-05-22 00:37:50 +0100

    Mailer adjustments

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
   default from: "noreply@sngtrkr.com"
-  ses = AWS::SES::Base.new(
-  :access_key_id     => ENV['AMAZON_ACCESS_KEY'],
-  :secret_access_key => ENV['AMAZON_SECRET_KEY']
-  )
   def welcome_email(user)
+    ses = AWS::SES::Base.new(
+    :access_key_id     => ENV['AMAZON_ACCESS_KEY'],
+    :secret_access_key => ENV['AMAZON_SECRET_KEY']
+    )
     ses.addresses.verify(user.email)
     mail(:to => user.email, :subject => "Welcome to SNGTRKR!")
   end
