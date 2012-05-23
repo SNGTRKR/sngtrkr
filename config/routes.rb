77ead0d22bc721543fae2608c240b18ff05304c5
Billy Dallimore <billy@sngtrkr.com>
2012-05-23 19:05:25 +0100

    Added loading page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ SNGTRKR::Application.routes.draw do
   match '/help' => "Pages#help"
   match '/recommended' => "Pages#recommended"
   match '/beta' => "Pages#beta"
-
+  match '/loading' => "Pages#loading"
+  
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin' # Feel free to change '/admin' to any namespace you need.
 
   devise_for :users, :controllers => { :registrations => "users/registrations", :omniauth_callbacks => "users_controller/omniauth_callbacks" }
