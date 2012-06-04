e14d548808a1cc68b66e917df85006e382974d66
Billy Dallimore <billy@sngtrkr.com>
2012-06-04 01:14:15 +0100

    Timeline changes

--- app/models/release.rb
+++ app/models/release.rb
@@ -2,7 +2,10 @@ class Release < ActiveRecord::Base
   validates :name, :presence => true
   validates :date, :presence => true
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { 
+  :medium => "300x300>", 
+  :thumb => "100x100>",
+  :release => "210x210#" }
 
   has_many :tracks
   belongs_to :artist
