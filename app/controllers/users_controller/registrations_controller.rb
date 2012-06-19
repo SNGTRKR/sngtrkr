803e8d2df5aac26e478b837991b9deb2aae51654
Matt Bessey <matt@sngtrkr.com>
2012-06-19 13:48:10 +0100

    Setting up soft delete. It's not holding the leave reason, and I'm not sure why, but I'll come back to this another time as storing leave reasons is less pressing than other tasks atm!

--- app/controllers/users_controller/registrations_controller.rb
+++ app/controllers/users_controller/registrations_controller.rb
@@ -5,4 +5,11 @@ class UsersController::RegistrationsController < Devise::RegistrationsController
   def check_permissions
     authorize! :create, resource
   end
+
+  def destroy
+    resource.soft_delete
+    set_flash_message :notice, :destroyed
+    sign_out_and_redirect(resource)
+  end
+
 end
