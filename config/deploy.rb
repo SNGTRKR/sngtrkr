ce942c62f0c646de8a2e5ae706c362ee81206624
Matt Bessey <matt@sngtrkr.com>
2012-06-30 23:15:14 +0100

    Deploy should now run paperclip when necessary

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,6 +51,16 @@ namespace :deploy do
   end
 end
 
+# Paperclip regeneration
+namespace :deploy do
+  desc "build missing paperclip styles"
+  task :build_missing_paperclip_styles, :roles => :app do
+    run "cd #{release_path}; RAILS_ENV=production bundle exec rake paperclip:refresh:missing_styles"
+  end
+end
+
+after("deploy:update_code", "deploy:build_missing_paperclip_styles")
+
 ##
 # Rake helper task.
 # http://pastie.org/255489
