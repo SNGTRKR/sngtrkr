7fbe6471dfc220b074cc8a287ef4423f54e0fb77
Matt Bessey <matt@sngtrkr.com>
2012-07-01 04:48:38 +0100

    Added track listing Scrollable Pulled scroll bar css out into application, just use @extend .scrollbar; to use it.

--- app/models/track.rb
+++ app/models/track.rb
@@ -5,5 +5,8 @@ class Track < ActiveRecord::Base
     require 'open-uri'
     Hash.from_xml( open( URI.parse("http://api.7digital.com/1.2/track/preview?trackid=#{sd_id}&oauth_consumer_key=7dufgm34849u&redirect=false")))["response"]["url"]
   end
-
+  
+  def self.ordered
+    order('number')
+  end
 end
