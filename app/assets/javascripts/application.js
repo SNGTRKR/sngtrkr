8bad96648ef1dcde3e2f17d833b327a0cbd5c91a
Matt Bessey <matt@sngtrkr.com>
2012-07-05 02:10:37 +0100

    Timeline setup pulled into separate JS file which is not loaded for other pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,77 +20,91 @@
 //= require jquery.purr
 //= require best_in_place
 //= require rotate/jQueryRotate.2.2
-//= require jquery.scrollto-1.4.2-min
-//= require timeline
 
 String.prototype.commafy = function () {
-	return this.replace(/(^|[^\w.])(\d{4,})/g, function($0, $1, $2) {
-		return $1 + $2.replace(/\d(?=(?:\d\d\d)+(?!\d))/g, "$&,");
-	});
+  return this.replace(/(^|[^\w.])(\d{4,})/g, function ($0, $1, $2) {
+    return $1 + $2.replace(/\d(?=(?:\d\d\d)+(?!\d))/g, "$&,");
+  });
 }
 
 Number.prototype.commafy = function () {
-	return String(this).commafy();
+  return String(this).commafy();
 }
 
 
-$(document).ready(function() {
+$(document).ready(function () {
+  // Release carousel
+  $(".artist-list").carouFredSel({
+    circular: false,
+    infinite: false,
+    auto: false,
+    scroll: {
+      items: "page"
+    },
+    prev: {
+      button: "#rel_prev",
+      key: "left"
+    },
+    next: {
+      button: "#rel_next",
+      key: "right"
+    },
+  });
 
   // Flash Dismissal
-  $('.flash-outer').delay(300).slideDown(500,'easeInQuad');
-  $('#flash-dismiss').click(function(){
-    $('.flash-outer').slideUp(500,'easeInQuad');
+  $('.flash-outer').delay(300).slideDown(500, 'easeInQuad');
+  $('#flash-dismiss').click(function () {
+    $('.flash-outer').slideUp(500, 'easeInQuad');
   })
-  if($('.flash-outer').data('disappear-after')){
+  if ($('.flash-outer').data('disappear-after')) {
     console.log("delay detected");
-    $('.flash-outer').delay($('.flash-outer').data('disappear-after')).slideUp(1000,'easeInQuad');
+    $('.flash-outer').delay($('.flash-outer').data('disappear-after')).slideUp(1000, 'easeInQuad');
   }
 
   // Release rating.
-  $('a').live('ajax:complete', function(xhr, status) {
+  $('a').live('ajax:complete', function (xhr, status) {
     $(".ajaxful-rating-wrapper").replaceWith(status.responseText)
   });
-  
-	$("#delete-account a").fancybox({
-		width : 431,
-		height : 286,
-		autoSize	: false,
-		closeClick	: false
-	});
-	$("#delete-artist-trigger a").fancybox({
-		width : 431,
-		height : 286,
-		autoSize	: false,
-		closeClick	: false
-	});
-	$(".release-menu a").fancybox({
-		width : 543,
-		height : 315,
-		autoSize	: false
-	});
-	$("#artist-bio a").fancybox({
-		autoScale	: true,
-	});
-	$("#manage-top a").fancybox({
-		autoScale	: true,
-	});
+
+  $("#delete-account a").fancybox({
+    width: 431,
+    height: 286,
+    autoSize: false,
+    closeClick: false
+  });
+  $("#delete-artist-trigger a").fancybox({
+    width: 431,
+    height: 286,
+    autoSize: false,
+    closeClick: false
+  });
+  $(".release-menu a").fancybox({
+    width: 543,
+    height: 315,
+    autoSize: false
+  });
+  $("#artist-bio a").fancybox({
+    autoScale: true,
+  });
+  $("#manage-top a").fancybox({
+    autoScale: true,
+  });
   /*$('#artist-mini-search-submit').click(function(){
     $(this).closest('form').submit();
     return false;
   });*/
-  $('#mini_search_field').keyup(function(){ $.get("/artists/search.json", { search: $('#mini_search_field').val() }, 
-    function(data){
-      if(data == null){
+  $('#mini_search_field').keyup(function () {
+    $.get("/artists/search.json", {
+      search: $('#mini_search_field').val()
+    }, function (data) {
+      if (data == null) {
         $('#mini_search_results').html("<a>Sorry, we found no results...</a>")
       } else {
         $('#mini_search_results').html("")
-        $.each(data, function(){ 
-          $('#mini_search_results').append("<li><a href=\"/artists/"+this.id+"\">"+this.name+"<br /><span>"+this.genre+"</span></a></li>");
+        $.each(data, function () {
+          $('#mini_search_results').append("<li><a href=\"/artists/" + this.id + "\">" + this.name + "<br /><span>" + this.genre + "</span></a></li>");
         });
       }
-    })}
-  );
+    })
+  });
 });
-
-    	
-
