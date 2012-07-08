4f4197b12da002b25daa2d1a0e0ff7d4acd94623
Matt Bessey <matt@sngtrkr.com>
2012-07-08 16:54:57 +0100

    Tweaked the deploy system so deploy:full is now necessary to compile assets/

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,6 +49,9 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+  task :full do
+    load 'deploy/assets'    
+  end
 end
 
 # Paperclip regeneration
