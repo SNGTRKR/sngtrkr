63695fa81acebdf308a1dfacef0809e662ac506e
Matt Bessey <matt@sngtrkr.com>
2012-07-09 03:59:11 +0100

    Offline recovery for track previews

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,7 +3,7 @@ class Track < ActiveRecord::Base
   belongs_to :release
   def preview
     require 'open-uri'
-    Hash.from_xml( open( URI.parse("http://api.7digital.com/1.2/track/preview?trackid=#{sd_id}&oauth_consumer_key=7dufgm34849u&redirect=false")))["response"]["url"]
+    Hash.from_xml( open( URI.parse("http://api.7digital.com/1.2/track/preview?trackid=#{sd_id}&oauth_consumer_key=7dufgm34849u&redirect=false")))["response"]["url"] rescue return false
   end
   
   def self.ordered
