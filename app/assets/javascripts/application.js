e8b3c9abcb4e11f3ba559bdb703cd874d656466f
Matt Bessey <matt@sngtrkr.com>
2012-07-14 20:40:03 +0100

    Switched twitter require order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,8 +20,8 @@
 //= require best_in_place
 //= require rotate/jQueryRotate.2.2
 // BOOTSTRAP REQUIRES
-//= require bootstrap-popover
 //= require bootstrap-tooltip
+//= require bootstrap-popover
 String.prototype.commafy = function () {
   return this.replace(/(^|[^\w.])(\d{4,})/g, function ($0, $1, $2) {
     return $1 + $2.replace(/\d(?=(?:\d\d\d)+(?!\d))/g, "$&,");
