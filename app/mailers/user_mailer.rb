6ca9175f28582a60869e94829e605c39d3193b9a
Matt Bessey <matt@sngtrkr.com>
2012-07-15 22:48:04 +0100

    Added the ability to test the mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,26 +11,40 @@ class UserMailer < ActionMailer::Base
   end
   
   def new_releases_deliver(frequency)
-    #frequency_enum = {:instant => 0, :daily => 1, :weekly => 2, :fortnightly => 3, :monthly => 4}
     User.where(:email_frequency => frequency).each do |user|
-      @user = user
-      @releases = user.release_notifications
-      if @releases.empty?
-        next
-      end
-      case frequency
-        when 1 then @date_adjective = "Daily"
-        when 2 then @date_adjective = "Weekly"
-        when 3 then @date_adjective = "Fortnightly"
-        when 4 then @date_adjective = "Monthly"
-      end
-      
-      # Building list of artist names for the email subject
-      artist_names = @releases.select('DISTINCT artist_id')[0,2].collect{|r| r.artist.name}.join(', ')
-      
-      mail(:to => "#{@user.first_name} #{@user.last_name} <#{@user.email}>", :subject => "#{@date_adjective} Update | New releases from #{artist_names}").deliver
-      user.release_notifications.destroy_all
+      user_releases(user,frequency)
     end
+  end
+
+  def new_releases(user,frequency)
+    @user = user
+
+    @releases = user.release_notifications
+
+    if ENV['TEST_MODE']
+      puts("Test mode active.")
+      @releases = Release.limit(10)
+    end
+
+    if @releases.empty?
+      return true
+    end
+    case frequency
+      when 1 then @date_adjective = "Daily"
+      when 2 then @date_adjective = "Weekly"
+      when 3 then @date_adjective = "Fortnightly"
+      when 4 then @date_adjective = "Monthly"
+    end
+    
+
+    # Building list of artist names for the email subject
+    artist_names = @releases.select('DISTINCT artist_id')[0,2].collect{|r| r.artist.name}.join(', ')
+    
+    mail(:to => "#{@user.first_name} #{@user.last_name} <#{@user.email}>", 
+      :subject => "#{@date_adjective} Update | New releases from #{artist_names}").deliver
+    user.release_notifications.destroy_all
+    return true
+
   end
   
   def daily_releases
