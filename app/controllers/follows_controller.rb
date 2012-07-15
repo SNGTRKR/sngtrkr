67bdb074f650f465d8201750709a3c2b6b268e9e
Matt Bessey <matt@sngtrkr.com>
2012-07-15 04:18:14 +0100

    Added 7digital suggestions

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -9,8 +9,21 @@ class FollowsController < ApplicationController
 
   def create
     current_user.follow_artist params[:artist_id]
-    current_user.unsuggest_artist params[:artist_id]
+    current_user.unsuggest_artist(params[:artist_id])
+    
     @tracked_artist = Artist.find(params[:artist_id])
+
+    # Generate suggestions based on this
+    if @tracked_artist.sdid?
+      suggestions = Scraper.similar_artists_7digital(@tracked_artist.sdid)
+      suggestions.each do |suggestion|
+        if !Artist.where(:sdid => suggestion.id).empty?
+          current_user.suggest_artist(Artist.where(:sdid => suggestion.id).first.id)
+        end
+      end
+    end
+
+
     @artist = current_user.suggested[5] rescue nil
     respond_to do |format|
       format.html { redirect_to artist_path(:id => params[:artist_id]) }
