34d3853eb990686f5dbb7e143439dfe6027122ac
Matt Bessey <matt@sngtrkr.com>
2012-07-17 17:42:21 +0100

    improved itunes import and so renabled it

--- app/workers/release_job.rb
+++ app/workers/release_job.rb
@@ -15,7 +15,7 @@ class ReleaseJob
     Release.sdigital_import artist
     
     # STAGE 2 - ITUNES IMPORT
-    #Release.itunes_import artist
+    Release.itunes_import artist
       
     end_time = Time.now
     elapsed_time = end_time - start_time
