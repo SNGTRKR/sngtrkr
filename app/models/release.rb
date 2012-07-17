1b3c64d59088059fbbbd2fa54c519bd59aa3abb1
Matt Bessey <matt@sngtrkr.com>
2012-07-17 21:16:42 +0100

    Sidekiq restart should now behave

--- app/models/release.rb
+++ app/models/release.rb
@@ -197,7 +197,7 @@ class Release < ActiveRecord::Base
           next
         end
 
-        if(!best_artwork.is_a(String))
+        if(!best_artwork.is_a?(String))
           raise "Artwork Error: Release: '#{r.name}'. Expected String, actually got: '#{io.inspect}'"
         end
         io = open(best_artwork, :proxy => @proxy)
