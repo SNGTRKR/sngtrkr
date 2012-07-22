d9db3eaff137c40d7ff5414a3f888af3ac4f2dea
Matt Bessey <matt@sngtrkr.com>
2012-07-23 00:58:15 +0100

    Fixed ordering (no reversing)

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -4,7 +4,7 @@ class TimelineController < ApplicationController
 
   def index
     user_timeline = Timeline.new current_user.id
-    @timeline = user_timeline.user(params[:page]).reverse
+    @timeline = user_timeline.user(params[:page])
     respond_to  do |format|
       format.js { render :partial => "timeline/user_timeline.js" }
     end
