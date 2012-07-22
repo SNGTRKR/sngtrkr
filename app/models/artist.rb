b240f6b2a2fc62f9b5c293201faab7dafa955b59
Matt Bessey <matt@sngtrkr.com>
2012-07-22 18:35:05 +0100

    Reduced concurrency in production Artists and users delete all methods now work

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -13,9 +13,9 @@ class Artist < ActiveRecord::Base
     :friend_activity_artist => "87x87#",
     :friend_trkrs => "136x121#" }
 
-  has_many :follow, :dependent => :delete_all
-  has_many :suggest, :dependent => :delete_all
-  has_many :manage, :dependent => :delete_all
+  has_many :follow, :dependent => :destroy
+  has_many :suggest, :dependent => :destroy
+  has_many :manage, :dependent => :destroy
 
   has_many :followed_users, :through => :follow, :source => :user
   has_many :suggested_users, :through => :suggest, :source => :user
