2f912cab53b846ccfa293edb646bd9e1ccf8f45f
Billy Dallimore <billy@sngtrkr.com>
2012-07-26 01:28:10 +0100

    Version 2 mini updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,6 +22,7 @@
 //= require bootstrap-tooltip
 //= require bootstrap-popover
 //= require bootstrap-tab
+//= require bootstrap-transition
 String.prototype.commafy = function () {
   return this.replace(/(^|[^\w.])(\d{4,})/g, function ($0, $1, $2) {
     return $1 + $2.replace(/\d(?=(?:\d\d\d)+(?!\d))/g, "$&,");
