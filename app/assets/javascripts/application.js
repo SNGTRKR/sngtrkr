f839c1e0d02065f6945c7a94971c824e61089e50
Matt Bessey <matt@sngtrkr.com>
2012-08-06 20:56:01 +0100

    Tidied up header code into partial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery_ujs
-//= require jquery-ui
 //= require instant-search/instant-search
 //= require jplayer/jquery.jplayer
 //= require jplayer/jplayer.playlist
@@ -26,7 +13,6 @@
 //= require scrollbar/jquery.jscrollpane.min.js
 //= require scrollbar/jquery.mousewheel.js
 //= require scrollbar/scroll-startstop.events.jquery.js
-
 // Matt's requires
 //= require ajax-replacements
 
