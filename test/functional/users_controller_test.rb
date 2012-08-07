083981f455d873beb076c881c226807c6850b848
Matt Bessey <matt@sngtrkr.com>
2012-08-07 12:33:59 +0100

    User tests complete

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,45 +1,45 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
   setup do
     @user = users(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
-  end
+  test "should not create user" do
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create user" do
-    assert_difference('User.count') do
+    assert_no_difference('User.count') do
       post :create, :user => @user.attributes
     end
 
-    assert_redirected_to user_path(assigns(:user))
+    #assert_redirected_to user_path(assigns(:user))
+  end
+
+  test "should not show user" do
+    get :show, :id => users(:two)
+    assert_response :redirect, "doesn't show user if you aren't logged in"
   end
 
   test "should show user" do
-    get :show, :id => @user
+    sign_in :user, @user
+    get :show, :id => users(:two)
     assert_response :success
   end
 
   test "should get edit" do
+    sign_in :user, @user
     get :edit, :id => @user
     assert_response :success
   end
 
   test "should update user" do
-    put :update, :id => @user, :user => @user.attributes
-    assert_redirected_to user_path(assigns(:user))
+    sign_in :user, @user
+    put :update, :id => @user, :user => {:first_name => @user.first_name, :last_name => @user.last_name}
+    assert_redirected_to edit_user_path(assigns(:user))
   end
 
   test "should destroy user" do
+    sign_in :user, @user
     assert_difference('User.count', -1) do
       delete :destroy, :id => @user
     end
