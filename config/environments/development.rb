bc65ec8e7a156e9cfbce6487370793c270e17a92
Matt Bessey <matt@sngtrkr.com>
2012-08-10 15:47:20 +0100

    Commented out mailer caching stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,9 @@ SNGTRKR::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
-
+  
+  # Necessary for live updating mailer
+  #config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
