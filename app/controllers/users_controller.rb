8fd26e365fe19a4cc51f53aeb9982ad5ddaccda6
Matt Bessey <matt@sngtrkr.com>
2012-08-30 19:45:16 +0100

    Sign in without facebook completely implemented!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
   # GET /users
   # GET /users.json
-  before_filter :self_only, :except => [:show, :timeline, :self, :local_new, :create]
-  before_filter :authenticate_user!, :except => [:local_new]
-  
-  #load_and_authorize_resource :except => [:local_new]
+  before_filter :self_only, :except => [:show, :timeline, :self, :new, :create]
+  before_filter :authenticate_user!, :except => [:new]
+  load_and_authorize_resource :except => [:new]
 
   def index
     @users = User.limit(50).all
