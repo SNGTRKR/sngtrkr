fad4d338d9d6a4f55e17be22003d31f9a438c3b9
Matt Bessey <matt@sngtrkr.com>
2012-09-01 10:17:56 +0100

    index migrations

--- db/migrate/20120901085623_add_indexes_where_needed.rb
+++ db/migrate/20120901085623_add_indexes_where_needed.rb
@@ -1,7 +1,6 @@
 class AddIndexesWhereNeeded < ActiveRecord::Migration
   def up
-    add_index :releases, [:date,:itunes_id,:sd_id,:artist_id]
-    add_index :artists, [:itunes_id,:sdid]
+    add_index :releases, [:date,:artist_id]
   end
 
   def down
