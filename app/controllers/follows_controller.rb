c6d8aecbc0511a0cff6740eb12f31148f2dc3560
Matt Bessey <matt@sngtrkr.com>
2012-09-01 11:19:26 +0100

    Suggestions fix

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -17,7 +17,7 @@ class FollowsController < ApplicationController
       suggestions = Scraper.similar_artists_7digital(@tracked_artist.sdid)
       if suggestions
         suggestions.each do |suggestion|
-          if suggestion and suggestion["id"] and Artist.where(:sdid => suggestion["id"]).count > 0
+          if suggestion.is_a?(Hash) and Artist.where(:sdid => suggestion["id"]).count > 0
             current_user.suggest_artist(Artist.where(:sdid => suggestion["id"]).first.id)
           end
         end
