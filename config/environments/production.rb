af1b5ad25a0edfa68695606ef0a8252ec19291c4
Matt Bessey <matt@sngtrkr.com>
2012-09-01 19:49:39 +0100

    less heavy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,6 @@ SNGTRKR::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.assets.initialize_on_precompile = true
 
   config.assets.precompile += %w(rails_admin/rails_admin.css rails_admin/rails_admin.js)
   config.assets.precompile += %w(*/application.css */application.js)
