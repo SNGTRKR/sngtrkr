f0bdd03fc3e2a161f175678d165e3295e7418e9c
Matt Bessey <matt@sngtrkr.com>
2012-09-06 19:06:10 +0100

    Restore admin powers to maximum

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -37,7 +37,7 @@ class Ability
   
   def admin_permissions(user)
     user_permissions(user)
-    can :manage, [Artist,Release]
+    can :manage, :all
     can :access, :rails_admin   # grant access to rails_admin
     can :dashboard              # grant access to the dashboard
   end
