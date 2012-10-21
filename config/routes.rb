403973592d819571205375c0618ff2b6a18786eb
Matt Bessey <matt@sngtrkr.com>
2012-10-21 14:55:45 +0100

    Static 404 pages

--- config/routes.rb
+++ config/routes.rb
@@ -88,16 +88,6 @@ SNGTRKR::Application.routes.draw do
 
   resources :feedbacks
 
-  #resources :labels, :only => [:show] # Not implemented in v1 so no access to it
-
-  #  Use this line for production
-  #unless Rails.application.config.consider_all_requests_local
-  #   match '*not_found', to: 'errors#error_404'
-  #end
-
-  # Use this line to view error in development
-  match '*not_found', to: 'errors#error_404'
-
 # The priority is based upon order of creation:
 # first created -> highest priority.
 
