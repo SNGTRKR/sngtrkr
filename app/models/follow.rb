b68483ff5c509a16e8bf5a6246e94c0c6534aba9
Matt Bessey <matt@sngtrkr.com>
2013-02-09 18:54:45 +0000

    Was overzealous with destroys. Fb import only on first login or two now. Destroys unfollowed artists on unfollow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -7,6 +7,8 @@ class Follow < ActiveRecord::Base
 
   after_create :delete_suggestion
 
+  before_destroy :destroy_unfollowed_artist
+
   def delete_suggestion
     Suggest.where(:user_id => user_id, :artist_id => artist_id).destroy_all
   end
@@ -17,6 +19,12 @@ class Follow < ActiveRecord::Base
 
   def self.user_follows(user_id)
     find(:all, :conditions => ["user_id = '#{user_id}'"])
+  end
+
+  def destroy_unfollowed_artist
+    if Follow.where("artist_id = ?",self.artist_id).count == 1
+      artist.destroy
+    end
   end
 
 
