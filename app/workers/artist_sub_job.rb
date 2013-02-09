a59f794620685d3bebb1a6acfc1817814d6cdf4d
Matt Bessey <matt@sngtrkr.com>
2013-02-09 19:04:42 +0000

    gave up on offset. auto follow artists with explanation

--- app/workers/artist_sub_job.rb
+++ app/workers/artist_sub_job.rb
@@ -6,6 +6,7 @@ class ArtistSubJob
   def perform access_token, user_id, artist
     artist_scraper = ArtistScraper.new(:access_token => access_token, :user_id => user_id, :facebook_info => artist)
     a = artist_scraper.import
+    User.find(user_id).following << a
     a
   end
 
