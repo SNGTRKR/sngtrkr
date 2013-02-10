280d0af30a2857d57a5910848d77b4bce040e821
Matt Bessey <matt@sngtrkr.com>
2013-02-10 03:20:08 +0000

    Better caching paperclip defaults

--- config/initializers/paperclip_defaults.rb
+++ config/initializers/paperclip_defaults.rb
@@ -16,5 +16,6 @@ Paperclip::Attachment.default_options.update({
     :region => 'eu-west-1',
   },
   :fog_directory => "sngtrkr-#{BUCKET_ENV}",
-  :fog_public =>  true
+  :fog_public =>  true,
+  :fog_file         => {:cache_control => 315576000}
 })
