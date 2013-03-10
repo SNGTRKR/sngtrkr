cd4c3a834a27d140b5556b5f9034af9b864b5331
Matt Bessey <matt@sngtrkr.com>
2013-03-10 01:00:15 +0000

    Fixed potential routing error

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,9 +1,12 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-# ==> Mailer Configuration
-# Configure the e-mail address which will be shown in Devise::Mailer,
-# note that it will be overwritten if you use your own mailer class with default "from" parameter.
+  
+  Devise::SessionsController.layout "no_sidebar"
+
+  # ==> Mailer Configuration
+  # Configure the e-mail address which will be shown in Devise::Mailer,
+  # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = "noreply@sngtrkr.com"
 
   # Configure the class responsible to send e-mails.
