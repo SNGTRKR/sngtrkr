4afebbced4068e1893b4c0ec9e9659253a55b993
Matt Bessey <matt@sngtrkr.com>
2013-04-03 23:40:55 +0100

    Removed API keys

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -1,6 +1,6 @@
 class Scraper
-  
-  @sevendigital_apikey = "7dufgm34849u"
+
+  @sevendigital_apikey = ENV['SNGTRKR_7DIGITAL']  
   
   if Rails.env.production?
     @proxy = 'http://localhost:3128'
