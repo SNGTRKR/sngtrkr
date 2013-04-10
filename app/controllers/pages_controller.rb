c6753a1660cbc92d3b1bc0e9f664812916f9f29c
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-04-10 20:08:03 +0100

    Cache static and home pages
    Under 100ms homepage load time. Boom.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,24 @@
 class PagesController < ApplicationController
 
   skip_before_filter :authenticate_user!
-  #caches_action :home, :layout => false
+  caches_action :home
   caches_action :about, :layout => false
   caches_action :privacy, :layout => false
   caches_action :terms, :layout => false
 
-  def home
+  def home    
+    Rails.cache.clear
     if(user_signed_in?)
       flash.keep
       return redirect_to '/tl'
     end
-    @releases = Release.all(:limit => 66)
+    @releases = Rails.cache.fetch("homepage_releases", :expires_in => 24.hours) do
+      Release.all(:limit => 66)
+    end
   end
 
   def about
+    expire_action :action => :home
   
   end
   
