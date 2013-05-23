dfd21b1e04a0e0a975ff870aeebb18a76542de8c
Matt Bessey <matt@sngtrkr.com>
2013-05-23 18:23:07 +0100

    Paperclip image reprocessing script written.
    Will need to convert this to Carrierwave when we have switched in
    master.

---
+++ lib/admin_tools.rb
@@ -0,0 +1,39 @@
+require 'net/http'
+
+class AdminTools
+
+	# For reprocessing Paperclip images, when certain styles are missing.
+	# Had to do it myself because their rake seems to crash for unknown reasons.
+	# This also has the advantage of checking if the file exists rather than just assuming all don't.
+	# We assume if the large image is missing, we need to reprocess this item.
+	# AdminTools.reprocess_missing_images Release
+	# AdminTools.reprocess_missing_images Artist
+	def self.reprocess_missing_images klass
+		klass.find_each do |inst|
+			rep = false
+			puts "START #{inst.id}"
+			unless inst.image.present?
+				next
+			end
+			large_image = inst.image(:large)
+			begin
+				actual = open(large_image)
+			rescue
+				rep = true
+			end
+			if actual.is_a?(StringIO) or !rep
+				puts "SKIP #{inst.id}"
+				next
+			end
+
+			puts "REPROCESS #{inst.id}"
+
+			begin
+				inst.image.reprocess!
+			rescue
+				inst.image.destroy
+			end
+		end
+	end
+
+end
