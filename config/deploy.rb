0240412292f5a1aa5630a535635781c6f141893b
Matt Bessey <matt@sngtrkr.com>
2013-05-26 16:16:20 +0100

    Switched to unicorn in production + multi stage deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,31 +1,25 @@
 set :application, "sngtrkr"
 set :user, "deploy"
-set :domain, 'sngtrkr.com'
-set :staging_domain, 'staging.sngtrkr.com'
 set :scm, 'git'
 set :repository,  "https://github.com/SNGTRKR/sngtrkr.git"
 set :scm_verbose, true
 
 set :applicationdir, "/var/www/sngtrkr"
-set :deploy_to, applicationdir
-set :branch, 'master'
+set(:deploy_to) { applicationdir }
 set :rvm_ruby_string, "2.0"
 set :rvm_type, :user
 
-role :web, domain                          # Your HTTP server, Apache/etc
-role :app, domain                          # This may be the same as your `Web` server
-role :db,  domain, :primary => true        # This is where Rails migrations will run
+role(:web) { domain }                          # Your HTTP server, Apache/etc
+role(:app) { domain }                          # This may be the same as your `Web` server
+role(:db) { domain }        # This is where Rails migrations will run
+
+set :stages, %w(production staging)
+set :default_stage, "staging"
+require 'capistrano/ext/multistage'
 
 # Bundler for remote gem installs
 require "rvm/capistrano"
 require "bundler/capistrano"
-
-# Sidekiq
-require "sidekiq/capistrano"
-
-# Whenever for cron jobs
-set :whenever_command, "bundle exec whenever"
-require "whenever/capistrano"
 
 # Only keep the latest 3 releases
 set :keep_releases, 3
@@ -36,24 +30,6 @@ set :deploy_via, :remote_cache
 
 # additional settings
 default_run_options[:pty] = true  # Forgo errors when deploying from windows
- 
-# Generate an additional task to fire up the thin clusters
-namespace :deploy do
-  desc "Start the Thin processes"
-  task :start do
-    run "cd #{current_path} && bundle exec thin start -C thin.yml"
-  end
-
-  desc "Stop the Thin processes"
-  task :stop do
-    run "cd #{current_path} && bundle exec thin stop -C thin.yml"
-  end
-
-  desc "Restart the Thin processes"
-  task :restart do
-    run "cd #{current_path} && bundle exec thin restart -C thin.yml"
-  end
-end
  
 namespace :solr do
   desc "start solr"
@@ -81,5 +57,3 @@ namespace :solr do
     run "ln -s #{shared_path}/solr/pids/ #{release_path}/solr/pids"
   end
 end
- 
-after "deploy:update_code", "solr:symlink"
---
+++ config/deploy/staging.rb
@@ -0,0 +1,4 @@
+set :domain, 'staging.sngtrkr.com'
+set :branch, 'staging'
+set :applicationdir, "/var/www/sngtrkr_staging"
+set :deploy_to, "/var/www/sngtrkr_staging"
