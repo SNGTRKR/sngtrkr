edd29c27023c084e1dd91a51851f036c184c796d
Matt Bessey <matt@sngtrkr.com>
2013-06-24 13:35:05 +0100

    Simplified ArtistSubJob
    Unnecessary duplication of following / suggest code removed, replaced
    with existing method from Scraper2

--- app/workers/artist_sub_job.rb
+++ app/workers/artist_sub_job.rb
@@ -12,26 +12,12 @@ class ArtistSubJob
   def perform opts 
     # Optional hash parameters
     opts.symbolize_keys! # Necessary as Redis stringifies keys.
-    opts.reverse_merge!(first_time: false) 
     if opts[:artist].nil?
       raise "Scrape error: :fb_data not set. Hash: #{opts}"
     end
     # Returns false when scrape fails
-    if artist = Scraper2.scrape_artist(fb_data: opts[:artist])      
-      if artist.save
-        follow_or_suggest_artist opts[:user_id], artist.id, opts[:first_time]
-        ReleaseJob.perform_async(artist.id)
-      end
-    end
-  end
-
-  private
-
-  def follow_or_suggest_artist user_id, artist_id, first_time
-    if first_time
-      Follow.create(user_id: user_id, artist_id: artist_id)
-    else
-      Suggest.create(user_id: user_id, artist_id: artist_id)
+    if artist = Scraper2.import_artist(fb_data: opts[:artist], first_time: opts[:first_time])
+      ReleaseJob.perform_async(artist.id)
     end
   end
 
