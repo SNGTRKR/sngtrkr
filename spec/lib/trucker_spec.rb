7d338922ab752490fd4a5b15a53d207aad110895
Matt Bessey <matt@sngtrkr.com>
2013-07-16 23:33:19 +0100

    Fixed Trucker spec failing with certain seeds

--- spec/lib/trucker_spec.rb
+++ spec/lib/trucker_spec.rb
@@ -7,36 +7,37 @@ describe Trucker do
   end
 
   before(:each) do
-    @mailer = mock
+    @mailer = double("Mail::Message")
     @mailer.stub(:deliver) { true }
     UserMailer.stub(:new_releases_email) { @mailer }
-    @user = create(:user)
-    @artist = create(:artist)
   end
+
+  let(:user) { create(:user) }
+  let(:artist) { create(:artist) }
+  let(:release) { create(:release, artist: artist, date: 1.day.ago) }
 
   context "when a release is added" do
 
     it "emails followers" do
-      @user.followed_artists << @artist
-      create(:release, artist: @artist)
+      user.followed_artists << artist
+      release
       @mailer.should_receive(:deliver).once
-      Trucker.deliver_new_releases_email(@user)
+      Trucker.deliver_new_releases_email(user)
     end
 
     it "does not email non followers" do
-      @user.followed_artists << @artist
+      user.followed_artists << artist
       user2 = create(:user)
-      create(:release, artist: @artist)
+      release
       @mailer.should_not_receive(:deliver)
       Trucker.deliver_new_releases_email(user2)
     end
 
     it "marks sent notifications as sent" do
-      Notification.count.should eq 0
-      @user.followed_artists << @artist
-      create(:release, artist: @artist)
+      user.followed_artists << artist
+      release
       Notification.first.sent.should eq false
-      Trucker.deliver_new_releases_email(@user)
+      Trucker.deliver_new_releases_email(user)
 
       Notification.first.sent.should eq true
       sent_ago = Time.now - Notification.first.sent_at
@@ -44,34 +45,33 @@ describe Trucker do
     end
 
     it "leaves unsent notifications as unsent" do
-      Notification.count.should eq 0
-      @user.followed_artists << @artist
+      user.followed_artists << artist
       user2 = create(:user)
-      user2.followed_artists << @artist
-      create(:release, artist: @artist)
-      Notification.where(:sent => true).length.should eq 0
-      Trucker.deliver_new_releases_email(@user)
+      user2.followed_artists << artist
+      release
+      expect(Notification.where(:sent => true).count).to eq 0
+      Trucker.deliver_new_releases_email(user)
 
       Notification.unsent.first.user.should eq user2
-      Notification.sent.first.user.should eq @user
+      Notification.sent.first.user.should eq user
     end
 
   end
 
   context "when there are no new releases" do
     it "does not email anyone" do
-      @user.followed_artists << @artist
+      user.followed_artists << artist
       @mailer.should_not_receive(:deliver)
-      Trucker.deliver_new_releases_email(@user)
+      Trucker.deliver_new_releases_email(user)
     end
 
     it "does not resend sent notifications" do
-      @user.followed_artists << @artist
-      create(:release, artist: @artist)
+      user.followed_artists << artist
+      release
       @mailer.should_receive(:deliver).once
-      Trucker.deliver_new_releases_email(@user)
+      Trucker.deliver_new_releases_email(user)
       # Resend again, but now with notification marked as sent
-      Trucker.deliver_new_releases_email(@user)
+      Trucker.deliver_new_releases_email(user)
     end
   end
 
