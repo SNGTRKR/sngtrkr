582b34e447fa75504b66aea0bdd67b31440dc3de
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-08-13 22:49:51 +0100

    Fixed issue with share popover in artist tile. Im an idiot

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,11 +19,11 @@ module ApplicationHelper
   def follow_button_small artist
     if user_signed_in?
       if current_user.following? artist.id
-        return link_to(artist_unfollow_path(artist, format: :js), :class => "remove-trkr-small btn btn-small active follow-#{artist.id}", :remote => true, :'data-id' => artist.id) do
+        return link_to(artist_unfollow_path(artist, format: :js), :class => "remove-trkr-small btn btn-small active", :remote => true, :'data-id' => artist.id) do
           'Tracked'
         end.html_safe
       else
-        return link_to(artist_follow_path(artist, format: :js), :class => "add-trkr-small btn btn-small follow-#{artist.id}", :remote => true, :'data-id' => artist.id) do
+        return link_to(artist_follow_path(artist, format: :js), :class => "add-trkr-small btn btn-small", :remote => true, :'data-id' => artist.id) do
           'Track'
         end.html_safe
       end
