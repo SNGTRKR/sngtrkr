f569ff5eb1b11bc0d9452e1ceba14aeea83ed1b7
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-10-16 21:03:29 +0100

    Added suggest cleanup method and privacy/terms links to the signup/signin modals

--- app/models/suggest.rb
+++ app/models/suggest.rb
@@ -26,8 +26,14 @@ class Suggest < ActiveRecord::Base
     self.relevant.find(:all, :conditions => ["user_id = '#{user_id}' AND artist_id = '#{artist_id}'"])
   end
 
-  def self.user_suggested(user_id)
-    self.relevant.find(:all, :conditions => ["user_id = '#{user_id}'"])
+  #TODO: It would seem when moving databases around, somewhere along the line, a lot of the suggested artists for users were already being followed by the user. This method quickly removed any rouge suggests.
+  def self.remove_redundant_suggests
+    User.all.each do |user|
+      @follows = Follow.where(:user_id => user.id).all
+      @follows.each do |follow|
+        Suggest.where(:user_id => follow.user_id, :artist_id => follow.artist_id).destroy_all
+      end
+    end
   end
 
 end
