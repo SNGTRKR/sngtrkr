d36407065b29a701ccdb68d3d47f256c9a11b347
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-11-04 01:06:03 +0000

    Further development of the report functional test

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -1,12 +1,26 @@
 require 'spec_helper'
-describe Report do
-
-	before :all do
-		@report = create(:report)
+describe ReportsController do
+	describe "GET #new" do
+		it "assigns a new Report to @report"
+			get :new
+			
+		end
+		it "renders the :new template"
 	end
 
-	describe "#new" do
-		
+	describe "POST #create" do
+		context "with valid attributes" do
+			it "saves the new report in the database"
+			it "redirects to the artist page"
+		end
+		context "with invalid attributes" do
+			it "does not save the new report in the database"
+			it "re-renders the artist page"
+		end
 	end
 
+	describe "DELETE #destroy" do
+		it "removes the report from the database"
+		it "redirects to the administration page"
+	end
 end
