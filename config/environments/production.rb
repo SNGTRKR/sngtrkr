bad906ab650484985d9c2e050f20c55b0cdc6010
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-12-15 19:31:42 +0000

    Set asset pipeline fallback for production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ SNGTRKR::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Debugging on production
   config.assets.debug = false
