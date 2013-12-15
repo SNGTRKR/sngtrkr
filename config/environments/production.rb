9d0b7aeaace98b6a01c06aca116cabafee92f6af
JellyFishBoy <tom@ruckusmedia.co.uk>
2013-12-15 21:23:24 +0000

    Reverted image_tags on home page, added asset prefix configuration and fixed font face asset paths

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,11 +20,12 @@ SNGTRKR::Application.configure do
 
   config.action_controller.asset_host = "http://cdn%d.sngtrkr.com"
 
+  config.assets.prefix = "/assets"
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Debugging on production
   config.assets.debug = false
